95d3d28391d2ae3f7fd4a04ea7f597efbd7967b2
kyuubi-Yhp <south01village@gmail.com>
2026-02-13 21:26:32 +0300

    добавлена базовая стуктура страниц и первый компонент

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      asdasdas
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/AudioPlayer/AudioPlayer.jsx
@@ -0,0 +1,16 @@
+import { useRef, useState } from "react"
+
+
+
+export const AudioPlayer = () => {
+  const audioRef = useRef(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
