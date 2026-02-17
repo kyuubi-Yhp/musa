eba542c9f18dd8f0fab29be68e8cf35a15f3d41c
kyuubi-Yhp <south01village@gmail.com>
2026-02-17 15:07:08 +0300

    добавлено состояние и кнопка в загрузчике

--- src/Pages/LoaderAudio/LoaderAudio.jsx
+++ src/Pages/LoaderAudio/LoaderAudio.jsx
@@ -7,16 +7,68 @@ export const LoaderAudio = () => {
   const [objTreck, setObjTreck] = useState({
     name: '',
     nameArtist: '',
-    picture: '',
-    songTrack: ''
+    picture: null,
+    songTrack: null
   })
+
+  const handelInputChange = (e) => {
+    const { name, value } = e.target
+
+    setObjTreck(prev => ({
+      ...prev, [name]: value
+    }))
+  }
+
+  const handleFileChange = (e) => {
+    const { name, files } = e.target
+
+    setObjTreck(prev => ({
+      ...prev,
+      [name]: files[0]
+    }))
+  }
+
+  const handelLoaderTreck = () => {
+    
+  }
+
 
   return (
     <div className='Loader__audio-box'>
-      <input type="text" name="name" placeholder="Название трека" />
-      <input type="text" name="nameArtist" placeholder="Исполнитель" />
-      <input type="file" accept="image/*" />
-      <input type="file" accept="audio/*" />
+      <input type="text"
+        name="name"
+        placeholder="Название трека"
+        value={objTreck.name}
+        onChange={handelInputChange}
+      />
+
+      <input type="text"
+        name="nameArtist"
+        placeholder="Исполнитель"
+        value={objTreck.nameArtist}
+        onChange={handelInputChange}
+      />
+
+      <input
+        type="file"
+        name="picture"
+        accept="image/*"
+        onChange={handleFileChange}
+      />
+
+      <input
+        type="file"
+        name="songTrack"
+        accept="audio/*"
+        onChange={handleFileChange}
+      />
+
+      <button
+      onClick={handelLoaderTreck}
+      >
+        загрузить
+      </button>
+
     </div>
   )
 }
