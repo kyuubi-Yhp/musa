523392bef0a7d2a751dd994c74d8bc00b1d8980a
kyuubi-Yhp <south01village@gmail.com>
2026-02-23 01:16:39 +0300

    добавлена функция изменения пароля

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,13 +1,22 @@
+import { HiLockClosed } from "react-icons/hi";
 import './SignIn.css'
+import { useState } from "react";
 
 export const SignIn = () => {
 
-  
+const [isLocked, setIsLocked] = useState(true)
+  const handelisLockToddle = () => {
+    setIsLocked(prev => !prev)
+  }
+
   return (
     <div className="signin__box">
       <h2>войти</h2>
       <input className='sigin__input' placeholder='email' type="email" />
-      <input className='sigin__input' placeholder='password' type="password" />
+      <input className='sigin__input'  placeholder='password' type={isLocked ? 'password' : 'text'} />
+      <HiLockClosed className="lock"
+        onClick={handelisLockToddle}
+        color={isLocked ? "gray" : "green"} />
       <button>войти</button>
     </div>
   )
