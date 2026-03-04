2567ac020dbec7f820b8928f836be38a24c07637
kyuubi-Yhp <south01village@gmail.com>
2026-03-04 23:04:21 +0300

    добавлена код минимального сервера

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/userRoutes');
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+module.exports = app;
+
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
